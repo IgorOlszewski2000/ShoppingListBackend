f15c03f121d9e360ce94db6fbf9aca7900304007
Igor Olszewski <71021482+IgorOlszewski2000@users.noreply.github.com>
2022-04-22 12:42:32 +0200

    lists and list (db)

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,17 @@
+from email.policy import default
 from lists import db
 
 class Lists(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=50), nullable=False, unique=True)
+    lista = db.relationship('List', backref='owned_user', lazy=True)
+
+class List(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=50), nullable=False)
+    amount = db.Column(db.Integer(), nullable=False)
+    purchased = db.Column(db.Boolean(), default=False, nullable=False)
+    owner = db.Column(db.Integer(), db.ForeignKey('lists.id'))
+
+    def __repr__(self):
+        return f'List {self.name}'
--- lists/routes.py
+++ lists/routes.py
@@ -1,12 +1,14 @@
 from lists import app
 from flask import render_template
 from lists.models import Lists
+from lists.models import List
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     lists = Lists.query.all()
-    return render_template('home.html', lists=lists)
+    list= List.query.all()
+    return render_template('home.html', lists=lists, list=list)
 
 @app.route('/about')
 def about_page():
