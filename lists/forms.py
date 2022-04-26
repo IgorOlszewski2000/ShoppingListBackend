994267cf179db88862e97bb725bca64abd1e6eb0
Igor Olszewski <71021482+IgorOlszewski2000@users.noreply.github.com>
2022-04-26 21:59:20 +0200

    everything except the open list

--- lists/forms.py
+++ lists/forms.py
@@ -5,29 +5,29 @@ from wtforms import StringField, SubmitField, IntegerField, BooleanField
 from wtforms.validators import Length, DataRequired
 
 class ListsForm(FlaskForm):
-    listname = StringField(label='List Name', validators=[Length(min=1, max=50), DataRequired()])
-    submit = SubmitField(label='Add list')
+    listname = StringField(label='LIST NAME', validators=[Length(min=1, max=50), DataRequired()])
+    submit = SubmitField(label='ACCEPT')
 
 class ItemForm(FlaskForm):
-    itemname = StringField(label="Item Name", validators=[Length(min=1, max=50), DataRequired()])
-    itemamount = IntegerField(label="Amount", validators=[DataRequired()])
-    itempurchased = BooleanField(label=" uncheck-unpurchased, check-purchased")
-    itemowner = IntegerField(label="ID of list ", validators=[DataRequired()])
-    submit = SubmitField(label='Add item')
+    itemname = StringField(label="ITEM NAME", validators=[Length(min=1, max=50), DataRequired()])
+    itemamount = IntegerField(label="AMOUNT", validators=[DataRequired()])
+    itempurchased = BooleanField(label="UNCHECK-UNPURCHASED, CHECK-PURCHASED")
+    itemowner = IntegerField(label="ID OF LIST", validators=[DataRequired()])
+    submit = SubmitField(label='ACCEPT')
 
 class RemoveItemForm(FlaskForm):
-    removeitemid = IntegerField(label="ID of item to remove", validators=[DataRequired()])
-    submit = SubmitField(label="Remove Item")
+    removeitemid = IntegerField(label="ID OF ITEM TO REMOVE", validators=[DataRequired()])
+    submit = SubmitField(label="ACCEPT")
 
 class RemoveListForm(FlaskForm):
-    removelistid = IntegerField(label="ID of list to remove", validators=[DataRequired()])
-    submit = SubmitField(label="Remove List")
+    removelistid = IntegerField(label="ID OF LIST TO REMOVE", validators=[DataRequired()])
+    submit = SubmitField(label="ACCEPT")
 
 class ChangePurchasedForm(FlaskForm):
-    itempurchasedid = IntegerField(label="ID of item", validators=[DataRequired()])
-    submit = SubmitField(label="Accept")
+    itempurchasedid = IntegerField(label="ID OF ITEM TO CHANGE", validators=[DataRequired()])
+    submit = SubmitField(label="ACCEPT")
 
 class ChangeListsForm(FlaskForm):
-    changelistid = IntegerField(label="ID of list to edit", validators=[DataRequired()])
-    editname = StringField(label="New name of list", validators=[Length(min=1, max=50), DataRequired()])
-    submit = SubmitField(label="Accept")
+    changelistid = IntegerField(label="ID OF LIST TO EDIT", validators=[DataRequired()])
+    editname = StringField(label="NEW NAME OF LIST", validators=[Length(min=1, max=50), DataRequired()])
+    submit = SubmitField(label="ACCEPT")
